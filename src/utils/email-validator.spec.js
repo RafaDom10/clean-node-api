cac19515f0e74cf952726dd0c48dcad50cd3519c
Rafael Domingues <r-domingues@outlook.com>
2022-04-22 12:26:47 -0300

    ensure EmailValidator retrurns false with validator returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,12 +1,21 @@
+const validator = require('validator')
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
+
 describe('Email validator', () => {
   test('Should return true with validator returns true ', () => {
     const sut = new EmailValidator()
     const isEmailValid = sut.isValid('valid_email@emai.com')
     expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false with validator returns false ', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email@emai.com')
+    expect(isEmailValid).toBe(false)
   })
 })
